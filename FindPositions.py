489f4d28b2293ac5d1998a84ad0e8cd8199fdba8
jshultz00 <77994089+jshultz00@users.noreply.github.com>
2022-04-18 12:28:52 -0500

    Update FindPositions.py

--- FindPositions.py
+++ FindPositions.py
@@ -2,6 +2,10 @@ import os
 
 def get_needle():
   needle = [0, 0, 0, 0, 0, 0, 0]
+  cmd = "cd ~"
+  os.system(cmd)
+  cmd = "cd surgical_robotics_challenge"
+  os.system(cmd)
   cmd = "rostopic echo -n1 /ambf/env/Needle/State"
   needle_state = os.system(cmd)
   print(needle_state)
