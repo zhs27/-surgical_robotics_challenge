d66018bc33e2ffee1d48e456074df67a99945042
fcyxx4105 <fcyxx4105@gmail.com>
2022-04-20 16:23:49 +0000

    update_point

--- Point.py
+++ Point.py
@@ -1,5 +1,9 @@
 class Point:
-    def __init__(self, x_=0.0, y_=0.0, z_=0.0):
+    def __init__(self, x_=0.0, y_=0.0, z_=0.0, ro_=0.0, pi_=0.0, ya_=0.0, gr_=0.0):
         self.x = x_
         self.y = y_
         self.z = z_
+        self.ro = ro_
+        self.pi = pi_
+        self.ya = ya_
+        self.gr = gr_
